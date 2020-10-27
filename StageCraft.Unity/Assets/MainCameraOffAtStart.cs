9ce419871a8776c3df088cc80a92de5b73b10d59
Ted Barnett <mail@tedbarnett.com>
2020-10-27 14:39:43 -0400

    Improved face graphics, hide mainCamera at start

---
+++ StageCraft.Unity/Assets/MainCameraOffAtStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraOffAtStart : MonoBehaviour
+{
+    public GameObject mainCamera;
+    void Start()
+    {
+        mainCamera.SetActive(false);
+    }
+
+}
