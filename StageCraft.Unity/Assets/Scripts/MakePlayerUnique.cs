e9fa1880b576c08153a2a5c0720d39a7973c0ab0
Ted Barnett <mail@tedbarnett.com>
2020-11-03 17:25:18 -0500

    Enabled unique colors for each player (using script on XR Rig).  Problem: Realtime does not seem to update the colors so that other players see the same colors (or names).

--- StageCraft.Unity/Assets/Scripts/MakePlayerUnique.cs
+++ StageCraft.Unity/Assets/Scripts/MakePlayerUnique.cs
@@ -6,15 +6,27 @@ using TMPro;
 public class MakePlayerUnique : MonoBehaviour
 {
     public TextMeshPro playerName;
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerName.text = "Player " + Mathf.RoundToInt(Random.Range(0.0f, 100.0f));
-    }
+    public GameObject playerHeadModel;
+    public GameObject playerLeftHandModel;
+    public GameObject playerRightHandModel;
+    private Color playerColor;
+    Renderer m_HeadRenderer;
+    Renderer m_LeftHandRenderer;
+    Renderer m_RightHandRenderer;
 
-    // Update is called once per frame
-    void Update()
+
+    void Awake()
     {
         
+        playerColor = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1.0f, 1.0f);
+        playerName.text = "Actor " + Mathf.RoundToInt(Random.Range(0.0f, 100.0f));
+
+        m_HeadRenderer = playerHeadModel.GetComponent<Renderer>();
+        m_LeftHandRenderer = playerLeftHandModel.GetComponent<Renderer>();
+        m_RightHandRenderer = playerRightHandModel.GetComponent<Renderer>();
+        m_HeadRenderer.material.color = playerColor;
+        m_LeftHandRenderer.material.color = playerColor;
+        m_RightHandRenderer.material.color = playerColor;
     }
+
 }
