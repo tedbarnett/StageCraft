3c65892634d5384373cad11eeb6d44fc06e56cbf
Ted Barnett <mail@tedbarnett.com>
2020-11-03 16:30:59 -0500

    Show Player name above head ("Player " + Random number)

---
+++ StageCraft.Unity/Assets/Scripts/MakePlayerUnique.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MakePlayerUnique : MonoBehaviour
+{
+    public TextMeshPro playerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerName.text = "Player " + Mathf.RoundToInt(Random.Range(0.0f, 100.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
