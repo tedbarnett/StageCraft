7d24ea8cd2269bbeb8235448442c9fa7a2a5db10
Ted Barnett <mail@tedbarnett.com>
2020-11-03 20:12:23 -0500

    Working on "tractor beam" grab

---
+++ StageCraft.Unity/Assets/TractorBeam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TractorBeam : MonoBehaviour
+{
+
+    public void Update()
+    {
+        var interactor = GetComponent<XRBaseInteractor>();
+        var controller = GetComponent<XRController>();
+
+        XRGrabInteractable grabbable = interactor.selectTarget as XRGrabInteractable;
+
+        if (grabbable != null && grabbable.attachTransform != null)
+        {
+            InputDevice device = InputDevices.GetDeviceAtXRNode(controller.controllerNode);
+
+            device.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 thumbDirection);
+
+            float moveAwayAmount = thumbDirection.y;
+
+            Vector3 vectorToMoveObjectAlong = grabbable.attachTransform.parent.InverseTransformVector(controller.transform.forward);
+            grabbable.attachTransform.localPosition += moveAwayAmount * vectorToMoveObjectAlong;
+
+            MethodInfo unity_UpdateInteractorLocalPose = typeof(XRGrabInteractable).GetMethod("UpdateInteractorLocalPose", BindingFlags.NonPublic | BindingFlags.Instance);
+            unity_UpdateInteractorLocalPose.Invoke(grabbable, new object[] { interactor });
+        }
+
+    }
+}
