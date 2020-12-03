2167c8e36f65934d6fada0f48e78242fcbb2a1e0
Ted Barnett <mail@tedbarnett.com>
2020-12-02 20:23:32 -0800

    Removing all Normcore components: will add back carefully

---
+++ StageCraft.Unity/Assets/InstantiateAvatar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateAvatar : MonoBehaviour
+{
+    public GameObject LocalAvatarPrefab;
+    public Transform StartPosition;
+
+    void Start()
+    {
+        var playerAvatar = Instantiate(LocalAvatarPrefab, StartPosition.position, StartPosition.rotation);
+    }
+
+}
